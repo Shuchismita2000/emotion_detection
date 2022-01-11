e93a78ffea966a40abcffaf9560059ccc11981fb
SHUCHISMITA MALLICK <86481925+Shuchismita2000@users.noreply.github.com>
2022-01-11 22:09:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,8 +53,9 @@ if inp=='Input text':
         st.write(table)
         Emotion=[]
 if inp=='Input text file':
-    file =st.file_uploader('Upload your text file',type=['txt','.docx'])
+    
     if str(file.name) is not None:
+        file =st.file_uploader('Upload your text file',type=['txt','.docx'])
         if str(file.name).endswith('.txt'):
             a_file = open(str(file.name), "r")
             lines = a_file.read()
